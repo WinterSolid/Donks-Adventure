f5ccbb670c53cd15b4e0f130f9202bf8edbb913c
WinterSolid <58896705+WinterSolid@users.noreply.github.com>
2021-09-21 18:01:37 -0500

    UI and button logi
    deleted template for actions
    -touch begin completed
    -touch move completed
    TODO: touch ended - resetting on button position
    TODO: Character/Scene movements 
    -

--- Donk's Adventure/GameScene.swift
+++ Donk's Adventure/GameScene.swift
@@ -10,79 +10,65 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    //Nodes
+    var character: SKNode?
+    var joyStick: SKNode?
+    var joyStickKnob: SKNode?
     
+    //non action
+    var joyStickAction = false
+    
+    //Measurement
+    var knobRadius: CGFloat = 50.0
+    
+    //did move
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        character = childNode(withName: "Character")
+        joyStick = childNode(withName: "joyStick")
+        joyStickKnob = joyStick?.childNode(withName: "joyStickKnob")
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
+}
+// MARK: User Touch
+extension GameScene {
+    //User initial touch
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        for touch in touches {
+            if let joyStickKnob = joyStickKnob {
+                let location = touch.location(in: joyStick!)
+                joyStickAction = joyStickKnob.frame.contains(location)
+            }
+        }
+    
+    }
+    //User Movement
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let joyStick = joyStick else { return }
+        guard let joyStickKnob = joyStickKnob else { return }
+        
+        if !joyStickAction { return }
+        
+        //Distance of movement
+        for touch in touches {
+            
+            let position  = touch.location(in: joyStick)
+            let length = sqrt(pow(position.y, 2) + pow(position.x, 2))
+            let angle = atan2(position.y, position.x)
+            
+            if knobRadius > length {
+                joyStickKnob.position = position
+            }else {
+                joyStickKnob.position = CGPoint(x: cos(angle) * knobRadius, y: sin(angle) * knobRadius)
+            }
         }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
+    //User deinitiates touch
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        for touch in touche {
+            let joy
+        }
+        
     }
 }
